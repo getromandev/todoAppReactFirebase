476f6f21cfbedc38955c31a848af99f4a4876526
getromandev <getroman.dev@gmail.com>
2020-10-15 12:58:00 -0400

    stubbed out form and add state to form

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
 
 function App() {
+  // state is similiar to the brains capacity to hold short term memories.
+  const [toDo, setTodos] = useState([]);
+
   return (
     <div className="App">
       <h1>Hello World 🚀 </h1>
+      <input />
+      <button>Add Todo</button>
+
+      <ul>
+        <li>Make food 🥫 </li>
+        <li>Feed the fish 🐠 </li>
+      </ul>
+
     </div>
   );
 }
