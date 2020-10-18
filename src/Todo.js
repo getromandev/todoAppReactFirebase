bcf2b549d6f28744633ba2cc7eff536ee5fbad8e
getromandev <getroman.dev@gmail.com>
2020-10-18 03:20:59 -0400

    add list item material UI components to Todo component

--- src/Todo.js
+++ src/Todo.js
@@ -1,12 +1,20 @@
 import React from 'react'
+import './Todo.css';
+import { Avatar, List, ListItem, ListItemAvatar, ListItemText, ImageIcon } from '@material-ui/core'
 
 function Todo(props) {
     console.log('props ==>', props)
     return (
-        <div>
-          <li>{props.text}</li>  
-        </div>
+        <List className='todo_list'>
+            <ListItem>
+
+                <ListItemAvatar></ListItemAvatar>
+
+                <ListItemText primary={props.text} secondary='Wha?' />
+
+            </ListItem>
+        </List>
     )
 }
-
 export default Todo
+
